9cf6fe6772ba9b63c3ab0c710403a085b21f8b2e
Hanan <Hanan.bayazeed56@gmail.com>
2026-05-07 17:51:18 +0300

    build the dark mode provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/organisms/Navbar";
-
+import ThemeProvider from "@/providers/ThemeProvider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,17 +20,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Navbar />
-        {children}
+        <ThemeProvider>
+          <Navbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -2,11 +2,16 @@ import { Users } from "@/assets/icons/icons";
 import Icon from "../atoms/Icon";
 import Logo from "../atoms/logo";
 
+import { ThemeToggle } from "../atoms/ThemeButton";
+
 export default function Navbar() {
   return (
     <nav className="flex items-center gap-3 px-6 py-4 border-b">
       <Icon IconComponent={Users} size={32} color="primary" />
       <Logo />
+      <div className="ml-auto">
+        <ThemeToggle />
+      </div>
     </nav>
   );
 }
